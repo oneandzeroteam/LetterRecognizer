dc6a5f1d704eb54d38f1c635d5b8499e83e77dd5
pjo901018 <pjo901018@korea.ac.kr>
2017-03-20 22:59:01 +0900

    read_dataset까지 완성

--- train_script.py
+++ train_script.py
@@ -1,7 +1,12 @@
-from config.config import FTP_Parameter
+from config.config import FTP_Parameter, HYPARMS
 from FTP_Manager.downloadfromftp import downloadfromftp
+from Tensorflow_DataConverter import inputdata
 
 
 
 ftp_pwd = ''
 downloadfromftp(FTP_Parameter(ftp_pwd))
+
+data_sets = inputdata.read_image_datasets(HYPARMS.train_data_dir,
+                                          HYPARMS.test_data_dir,
+                                          reshape=False)
