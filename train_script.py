9262d7104457c3febaeaa795cf9bf1aedcf2d81e
pjo901018 <pjo901018@korea.ac.kr>
2017-03-21 19:58:14 +0900

    load한 데이터 visualize 추가

--- train_script.py
+++ train_script.py
@@ -10,6 +10,8 @@ downloadfromftp(FTP_Parameter(ftp_pwd))
 data_sets = inputdata.read_image_datasets(HYPARMS.train_data_dir,
                                           HYPARMS.test_data_dir,
                                           reshape=False)
+from Tensorflow_DataConverter.visualize.visualizer_numpy import show_random_nbyn_data
+show_random_nbyn_data(data_sets.train)
 
 model = Model()
 model.load_data(data_sets.train, data_sets.test)
