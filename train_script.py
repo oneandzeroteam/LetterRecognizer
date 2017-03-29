b1127d42b27e54c982f4124815c3a42737a45c6a
OAZ <triptych1003@gmail.com>
2017-03-29 13:04:36 +0900

    requirements pip>=9.0 추가, ftp 다운로드 주석 해제

--- train_script.py
+++ train_script.py
@@ -4,14 +4,14 @@ from Tensorflow_DataConverter import inputdata
 from LetterRecognizer.model import Model
 
 
-# ftp_pwd = ''
-# downloadfromftp(FTP_Parameter(ftp_pwd))
+ftp_pwd = ''
+downloadfromftp(FTP_Parameter(ftp_pwd))
 
 data_sets = inputdata.read_image_datasets(HYPARMS.train_data_dir,
                                           HYPARMS.test_data_dir,
                                           reshape=False)
-# from Tensorflow_DataConverter.visualize.visualizer_numpy import show_random_nbyn_data
-# show_random_nbyn_data(data_sets.train)
+from Tensorflow_DataConverter.visualize.visualizer_numpy import show_random_nbyn_data
+show_random_nbyn_data(data_sets.train)
 
 model = Model()
 model.load_data(data_sets.train, data_sets.test)
